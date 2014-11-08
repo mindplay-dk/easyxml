3087274f8cbeeef27aff162a86afb01ef37aee5b
Rasmus Schultz <rasc@fynskemedier.dk>
2014-11-08 15:45:30 +0100

    improved benchmark

--- benchmark.php
+++ benchmark.php
@@ -10,6 +10,8 @@ require __DIR__ . '/example/model.php';
 
 header('Content-type: text/plain');
 
+echo "Benchmarking, please wait...\n\n";
+
 $bench = new Benchmark();
 
 $path = __DIR__ . '/example/cd_catalog.xml';
@@ -26,5 +28,8 @@ $size = filesize($path);
 
 $total = $size * $iterations;
 
+$rate = ($total / 1024) / ($elapsed / 1000);
+
 echo "Parsed a {$size} bytes file {$iterations} times in {$elapsed} msec (average {$time} msec per operation)\n";
 echo "Total content parsed: {$total} bytes\n";
+echo "Average throughput: {$rate} KB/sec\n";
